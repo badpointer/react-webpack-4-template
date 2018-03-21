69f0019cbb04c38542929af6247b4593533b6d7f
Bryan Solero <arindil@gmail.com>
2018-03-21 14:14:01 -0500

    Added ESLint and configured for the project

--- src/components/AppContainer/AppContainer.js
+++ src/components/AppContainer/AppContainer.js
@@ -1,34 +1,31 @@
-import React, {Component} from 'react';
+import {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class AppContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            Hello: "Hello from within App Container!"
-        }
-    }
+  constructor (props) {
+    super(props);
+    this.state = {
+      Hello: 'Hello from within App Container!'
+    };
+  };
 
-    render() {
-        return (
-            <h1>
-                {this.state.Hello}
-                <br/>
-                {this.props.helloDefaultProp}
-            </h1>
-        );
-    }
-}
-
+  render () {
+    return (
+      <h1>
+        {this.state.Hello}
+        <br/>
+        {this.props.helloDefaultProp}
+      </h1>
+    );
+  };
+};
 
 AppContainer.defaultProps = {
-    helloDefaultProp: "Hello being a Default Prop"
-}
+  helloDefaultProp: 'Hello being a Default Prop'
+};
 
 AppContainer.propTypes = {
-    helloDefaultProp: PropTypes.string.isRequired
-}
-
-
+  helloDefaultProp: PropTypes.string.isRequired
+};
 
 export default AppContainer;
